acbff6881b34463631d39dab60d7f0c807c76a05
JoelEmati <jelemati@gmail.com>
2024-01-06 12:41:25 +0100

    initialisation

---
+++ src/main/java/com/ime/studentmobility/StudentmobilityApplication.java
@@ -0,0 +1,13 @@
+package com.ime.studentmobility;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentmobilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentmobilityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ime/studentmobility/StudentmobilityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ime.studentmobility;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentmobilityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
